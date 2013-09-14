cc202b4fd44b15aa3ef66cf1b4842554ace439bd
Nicholas Faiz <nicholas.faiz@gmail.com>
2013-09-14 16:37:49 +1000

    unescape what is sent through in nginx before postgres escaping the data

--- db/polyhub.sql
+++ db/polyhub.sql
@@ -5,6 +5,7 @@ create table members (
   moniker       varchar(100) not null unique,
   joined        date default CURRENT_DATE,
   data          json,
+  data2         text,
   updated_at    date default CURRENT_DATE,
   created_at    date default CURRENT_DATE
 );
